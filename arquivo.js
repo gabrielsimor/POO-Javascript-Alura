a084b536bef5e64287b8411429ffea5a5f6a933d
Gabriel Simor <simor.gabriel@gmail.com>
2022-10-07 21:02:36 -0300

    Initial Structure

---
+++ arquivo.js
@@ -0,0 +1,11 @@
+const cliente1Nome = 'Ricardo';
+const cliente1CPF = '111222333009';
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = 'Alice';
+const cliente2CPF = '888444777009';
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome)
