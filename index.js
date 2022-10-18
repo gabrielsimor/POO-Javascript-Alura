0db8673f6bb8989274626c63b296057d9b0fc28c
Gabriel Simor <simor.gabriel@gmail.com>
2022-10-18 14:41:39 -0300

    constructors

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1  = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = '111222333009';
+const cliente1  = new Cliente('Ricardo', 111222333009);
+const cliente2 = new Cliente('Alice', 888444777009);
 
-const cliente2 = new Cliente();
-cliente2.nome = 'Alice';
-cliente2.cpf = '888444777009';
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.depositar(200);
-    
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+contaCorrenteRicardo.depositar(500)
+const conta2 = new ContaCorrente(cliente2, 102);
 
 contaCorrenteRicardo.transferir(150, conta2);
 
